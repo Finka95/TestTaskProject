29a8022dc441658e716f0a005b18cf9c9585b511
Marshyn <infin95@gmail.com>
2022-04-04 09:16:47 +0300

    commit

---
+++ TestTaskProject/UnitTestApi.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+using System.Net.Http;
+
+namespace TestTaskProject
+{
+    public class UnitTestApi
+    {
+        
+    }
+}
